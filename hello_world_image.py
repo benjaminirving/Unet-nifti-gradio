677a3451e196f69875e22b90f031701ba7219519
Ben Irving <benjamin.irv@gmail.com>
2022-06-07 13:39:57 +0100

    add dropdown and second output

---
+++ hello_world_image.py
@@ -0,0 +1,18 @@
+import numpy as np
+import nibabel as nib
+import gradio as gr
+
+def sepia(input_img, dropdown):
+
+    # Loading a nifti in the traditional sense
+    img = nib.load(input_img.name)
+    data = img.get_fdata()
+    slice1 = np.squeeze(data[:, :, 200])
+    slice1_float = (slice1 - np.min(slice1)) /(np.max(slice1) - np.min(slice1)) 
+
+    return slice1_float, slice1_float
+
+
+demo = gr.Interface(sepia, ["file", gr.Dropdown(['UNET', 'UNETR'])], ["image", "image"], live=True)
+
+demo.launch()
